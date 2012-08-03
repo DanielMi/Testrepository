6202d33290e56a416e254e2d0c7de59e550cabd7
Daniel <daniel.mikus@ecards-and-more.de>
2012-08-03 13:59:54 +0200

    konflikt gelöst

--- class.Shoppingcard.php
+++ class.Shoppingcard.php
@@ -8,6 +8,9 @@ Daniel: Blabla1
 Daniel: Blabla1
 Daniel: Blabla1
 Daniel: Blabla1-----DAS IST MEINE ZEILE!!
+
+Daniel: Blablaad fasd fasd fasd fadsf 
+
 Daniel: Blabla1
 Daniel: Blabla1
 Daniel: Blabla3
