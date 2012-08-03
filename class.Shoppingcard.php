57d0c5abf15debb0cc0bffc4cbd92d2a2835a971
Daniel <daniel.mikus@ecards-and-more.de>
2012-08-03 11:23:04 +0200

    simultanes Bearbeiten START

---
+++ class.Shoppingcard.php
@@ -0,0 +1,12 @@
+
+
+Hier ein Test zum simultanen Bearbeiten einer Datei.
+
+Daniel: Blabla1
+Daniel: Blabla1
+Daniel: Blabla1
+Daniel: Blabla1
+Daniel: Blabla1
+Daniel: Blabla1
+Daniel: Blabla1
+Daniel: Blabla1
